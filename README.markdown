d6323583b56ca3ef0406252da418d4b96748ea81
Gabriel Horner <gabriel.horner@gmail.com>
2009-04-11 20:14:46 -0400

    added machine tag tree plugin + demo

--- README.markdown
+++ README.markdown
@@ -1,12 +1,14 @@
 Description
 ===========
-This jQuery plugin provides machine-tag related methods, most importantly a machine
-tag search. For an introduction to machine tags, [read this](http://tagaholic.me/2009/03/26/what-are-machine-tags.html).
+These jQuery plugins provides machine-tag related methods, most importantly a machine
+tag search and a machine tag tree. For an introduction to machine tags, [read this](http://tagaholic.me/2009/03/26/what-are-machine-tags.html). To see a live version of the machine tag tree see [my blog's search engine](http://tagaholic.me/blog.html).
 
 Setup
 =====
     <script src="jquery.js"></script>
     <script src="jquery.machineTag.js"></script>
+    <!-- optionally display results with machineTagTree -->
+    <script src="jquery.machineTagTree.js"></script>
 
 Usage
 =====
@@ -35,13 +37,36 @@ The wildcard syntax should be compatible with [flickr's wildcard search](http://
     mtag.predicate //=>'type'
     mtag.value     //=>'blog'
 
-Plugin Methods
-==============
-* machineTagSearch(wildcard\_machine\_tag, options): Returns machine-tagged items that match wildcard machine tag.
-* machineTagSearchRecordTags(wildcard\_machine\_tag, records): Returns tags from machine-tagged items that match the wildcard machine tag.
-* machineTagSearchLocation(options): Wrapper around machineTagSearch() which assumes wildcard is appended to the end of the url after a '#'
-  ie http://example.com/tag\_search#wildcard .
-* hideMachineTags(): Assuming text of selector elements are machine tags, replaces them with a
-  hidden namespace and predicate and visible value. The hidden fields can be toggled visible with
-  toggleHiddenMachineTags().
+Machine Tag Tree Plugin
+=======================
+jquery.machineTagTree.js is an optional jquery plugin that complements jquery.machineTag.js. It provides a tree view for visualizing machine-tagged items that match a machine tag query.
 
+Plugin API's
+==============
+These are brief descriptions of the methods. Read a plugin's source for more documentation.
+
+jquery.machineTag.js provides the following methods:
+* $.machineTagSearch(wildcard\_machine\_tag, options): Returns machine-tagged items that match wildcard machine tag.
+* $.machineTagSearchRecordTags(wildcard\_machine\_tag, records): Returns tags from machine-tagged items that match the wildcard machine tag.
+* $.machineTagSearchLocation(options): Wrapper around machineTagSearch() which assumes wildcard is appended to the end of the url after a '#'
+  ie http://example.com/tag\_search#wildcard .
+* $.hideMachineTags(): Assuming text of selector elements are machine tags, replaces them with a
+  hidden namespace and predicate and visible value. The hidden fields can be toggled visible with
+  $.toggleHiddenMachineTags().
+
+jquery.machineTagTree.js provides the following methods:
+* $.machineTagTree(wildcard\_machine\_tag, records, options): Given a wildcard machine tag and its matching records, displays the results in a tree table. Designed to be passed as a displayCallback to $.machineTagSearch(). Dependent on the jquery treeTable plugin.
+* $.createMachineTagTree(wildcard\_machine\_tag, records): Given a wildcard machine tag and its matching records, gives back an array of nodes to be displayed as a tree.  Designed to be passed as a displayCallback to $.machineTagSearch(). Not dependent on any plugins.
+
+
+Demo
+====
+The demo uses both plugins to display a machine tag tree. To try the demo:
+
+1. Be online since it uses google's jquery library.
+2. Copy jquery.machineTag.js and jquery.machineTagTree.js to demo/javascripts
+3. Open demo/index.html in your browser.
+
+License
+=======
+See LICENSE.txt for these plugins. The treeTable plugin bundled as a dependency is MIT.
---
+++ jquery.machineTagTree.js
@@ -0,0 +1,159 @@
+(function($) {
+  // Creates an array of tree node objects to display the results of a machine tag search.
+  // Nodes are either tag or record nodes. Tag nodes represent different parts of a machine tag (namespace, predicate, value).
+  // Each tree node can have the following attributes:
+  //  * level (all nodes): Indicates level in a tree. A top level has a value of 0.
+  //  * tag (tag node): Indicates a tag fields' value.
+  //  * mtag (tag node): A tag's machine tag.
+  //  * record_count (tag node): Record count under a tag. Only set for a value tag node.
+  //  * record (record node): Reference to record.
+  $.createMachineTagTree = function(wildcard_machine_tag, records) {
+    var machine_tags = $.machineTagSearchRecordTags(wildcard_machine_tag, records);
+    var rows = [];
+    $.each(machine_tags, function(i,tag) {
+      var mtag = $.machineTag(tag);
+      var tagged_records = $.grep(records, function(post, j) { return $.inArray(tag, post.tags) != -1});
+      var tag_rows = [{tag: mtag.namespace,mtag: mtag, level:0}, {tag: mtag.predicate,mtag: mtag, level:1}, 
+        {tag: mtag.value, mtag: mtag,level:2, record_count: $(tagged_records).size()}];
+      var tag_rows = $.grep(tag_rows, function(e) { 
+        return ! $.machineTag.any(rows, function(f) { return f.tag == e.tag && f.level == e.level && f.mtag.namespace == e.mtag.namespace &&
+          (e.level == 2 ? f.mtag.predicate == e.mtag.predicate : true)
+        });
+      });
+      $.each(tagged_records, function(j,e) { tag_rows.push({level: 3, record: e}); });
+      $.merge(rows, tag_rows);
+    });
+    return rows;
+  };
+
+  // Given a wildcard machine tag and its matching records, displays the results in a tree table. The tree organizes the records by the machine
+  // tags that match the wildcard machine tag. The table has three columns: 
+  //   * machine tags: displays machine tags that match wildcard and are the parent nodes in the tree
+  //   * record: displays record info, leaf nodes in a tree
+  //   * record tags: displays a record's machine tags
+  // This function takes the following options:
+  //   * recordName: capitalized name to give the records, defaults to Records
+  //   * tagTreeId: css id for the div containing the generated table, defaults to tag_tree
+  //   * tableId: css id for the generated table, defaults to machine_tag_table
+  //   * caption : caption/title for the table
+  //   * formatter : hash of columns to functions used to format the respective column,
+  //     hash keys are machine_tags_column, record_column, record_tags_column
+  $.machineTagTree = function(wildcard_machine_tag, records, options) {
+    var options = $.extend({recordName: 'Records', tagTreeId: 'tag_tree', tableId: 'machine_tag_table', 
+      wildcard_machine_tag: wildcard_machine_tag},$.machineTagTree.defaultOptions, options || {});
+    $('#'+options.tagTreeId).html('');
+    var rows = $.createMachineTagTree(wildcard_machine_tag, records);
+    displayMachineTagTree(rows, options);
+  }
+  $.machineTagTree.defaultOptions = {};
+
+  //private methods
+  function displayMachineTagTree(rows, options) {
+    if ($(rows).size() == 0) {
+      var table_html = "<div class='caption'>No " + options.recordName.toLowerCase() + " found for '" + options.wildcard_machine_tag+ "'</div>";
+      $("#"+options.tagTreeId).append(table_html);
+    }
+    else {
+      setupTreeTable(rows);
+      var table_html = createTable(rows, options);
+      $("#"+options.tagTreeId).append(table_html);
+      $("a.machine_tag_search").hideMachineTags();
+      $("a.machine_tag_search").click(function() { $.machineTagSearch($(this).text());});
+      $("a.machine_tag_href_search").click(function() { $.machineTagSearch(this.href.match(/#(.*?)$/).pop());});
+      $("#"+options.tableId).treeTable({initialState: "expanded", indent:15});
+    }
+  };
+
+  function createTable(rows, options) {
+    var options = $.extend({ recordName: 'Records', tableId: 'machine_tag_table', caption: 'Machine Tag Search Results', 
+      formatter: { record_column: recordColumnFormatter, record_tags_column: recordTagsColumnFormatter,
+      machine_tags_column: machineTagsColumnFormatter}
+      }, options || {});
+    if (options.wildcard_machine_tag) options.caption = options.recordName+" for wildcard machine tag '"+
+      options.wildcard_machine_tag +"'";
+    var table = "<table id='"+options.tableId+"'><caption>"+options.caption+"</caption>\
+    <thead>\
+      <tr>\
+        <th width='140'>Machine Tags <a href='javascript:void($(\"tr[level=2]\").each(function(){$(this).toggleBranch()}))'>\
+        (Collapse/Expand)</a></th>\
+        <th>"+ options.recordName +"</th>\
+        <th>"+singularize(options.recordName)+" Tags / <a href='javascript:void($.toggleHiddenMachineTags())'>Machine Tags</a></th>\
+      </tr>\
+    </thead><tbody>" +
+    $.map(rows, function(e,i) {
+      return "<tr id='"+ e.id + "'" + (typeof e.parent_id != 'undefined' ? " class='child-of-"+e.parent_id+"'" : '' ) +
+        "level='"+e.level+"'>" +
+        "<td>"+(e.tag ? options.formatter.machine_tags_column.call(this, e) : '')+"</td>"+
+        "<td>"+(e.record ? options.formatter.record_column.call(this, e.record) : '')+"</td>" +
+        "<td>"+(e.record ? options.formatter.record_tags_column.call(this, e.record) : '')+"</td>" +
+        "</tr>";
+    }).join(" ") + "</tbody></table>";
+    return table;
+  };
+
+  function machineTagsColumnFormatter(row) {
+    var link_text = row.tag + (row.record_count ? " ("+row.record_count+")" : '');
+    return "<a class='machine_tag_href_search' href='#" + machineTagQuery(row) + "'>"+ link_text + "</a>";
+  }
+
+  function recordColumnFormatter(record) {
+    return "<a href='"+record.url+"'>"+truncate(record.title || record.url, 50)+"</a>";
+  }
+
+  function recordTagsColumnFormatter(record) {
+    return "<div class='record_tags_column'>"+createTagLinks(record.tags)+"</div>";
+  }
+
+  function createTagLinks(tags) {
+    return $.map(tags, function(f) {
+      return "<a class='machine_tag_search' href='#"+ f+"'>" + f + "</a>";
+    }).join(', ');
+  };
+
+  function singularize(string) {
+    return string.replace(/s$/,'')
+  };
+
+  function truncate(string,length) {
+    return (string.length > length)  ? string.slice(0, length - 3) + '...' : string;
+  }
+
+  function machineTagQuery(tree_node) {
+    var href = null;
+    var mtag = tree_node.mtag;
+    var base_href = tree_node.mtag.namespace+ $.machineTag.predicate_delimiter;
+    switch (tree_node.level) {
+      case 0: href = base_href + "*"; break;
+      case 1: href = base_href + mtag.predicate + $.machineTag.value_delimiter + "*"; break;
+      default: href = base_href+ mtag.predicate + $.machineTag.value_delimiter + mtag.value;
+    }
+    return href;
+  }
+
+  // Adds parents + ids
+  function setupTreeTable(array) {
+    $(array).each(function(i,e) {
+      e.id = "node-"+ i;
+    });
+    $(array).each(function(i,e) {
+      if (parent = $.grep(array.slice(0, i).reverse(), 
+        function(f) { return f.level < e.level})[0]) {
+          e.parent_id = parent.id;
+      }
+    });
+    $(array).each(function(i,e) {
+      if (e.level < 2) {
+        var record_count_sum = 0;
+        var child_index = i + 1;
+        while(array[child_index] && e.level < array[child_index].level) {
+          if (array[child_index].record_count) {
+            record_count_sum += array[child_index].record_count;
+          }
+          child_index += 1;
+        }
+        if (record_count_sum > 0) { e.record_count = record_count_sum; }
+      }
+    });
+    return;
+  };
+})(jQuery);
